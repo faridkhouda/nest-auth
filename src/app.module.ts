b5944dffadd3c41b879d307d2208e50bbf1f2e89
faridkhouda <geekamazighe@gmail.com>
2022-04-14 15:48:03 +0200

    add mnogo connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest')],
+  imports: [MongooseModule.forRoot('mongodb+srv://AdminDB:AdminDB@serverlessinstance0.kc3wc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')],
 })
 export class AppModule {
   const test
